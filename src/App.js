2e53845f16ff20f9ac64ffe722cc28c249e5dcf1
wildenali <wildenwildenaliali@gmail.com>
2020-08-10 17:33:04 +0700

    02 - Button dan cara menggunakan elemet dasar

--- src/App.js
+++ src/App.js
@@ -12,20 +12,20 @@
 // // --------------------- 1 Basic Template ---------------------
 
 
-// // --------------------- 2 - Button dan element basic ---------------------
-// import React from 'react';
-// import './App.css';
-// function App() {
-//   const judul = "Ini Judul"
-//   const handlePencet = (e) => {
-//     console.log('Di pencet')
-//   }
-//   return (
-//     <div className="App">
-//       <h1>{judul}</h1>
-//       <button onClick={handlePencet}>Pencet</button>
-//     </div>
-//   );
-// }
-// export default App;
-// // --------------------- 2 - Button dan element basic ---------------------
+// --------------------- 2 - Button dan element basic ---------------------
+import React from 'react';
+import './App.css';
+function App() {
+  const judul = "Ini Judul"
+  const handlePencet = (e) => {
+    console.log('Di pencet')
+  }
+  return (
+    <div className="App">
+      <h1>{judul}</h1>
+      <button onClick={handlePencet}>Pencet</button>
+    </div>
+  );
+}
+export default App;
+// --------------------- 2 - Button dan element basic ---------------------
