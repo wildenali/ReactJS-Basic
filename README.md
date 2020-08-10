dfc5af3f6a28927851b4bec7a8b80a077a329715
wildenali <wildenwildenaliali@gmail.com>
2020-08-10 17:14:45 +0700

    01 - React template

--- README.md
+++ README.md
@@ -26,3 +26,6 @@ The build is minified and the filenames include the hashes.<br />
 Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+
+# // --------------------- 1 Basic Template ---------------------
+Cara menggunakan react template
--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+// --------------------- 1 Basic Template ---------------------
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <h1>Test 123</h1>
     </div>
   );
 }
-
 export default App;
+// --------------------- 1 Basic Template ---------------------
